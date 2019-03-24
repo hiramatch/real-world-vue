5a33c2ee78bbd0a3f6dbdab4d7463cef9cc241a0
hiramatch <hiramatch@gmail.com>
2019-03-24 16:45:42 +0900

    Single Vue File  Component

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Events Listing</h1>
+  <div>
+    <h1>Events Listing</h1>
+    <router-link class="event-link" :to="{ name: 'event-show', params: { id: '1' } }">
+      <event-card/>
+    </router-link>
+  </div>
 </template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
