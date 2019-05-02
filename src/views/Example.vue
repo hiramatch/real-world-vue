41729aeb431f9e5114d694d76ff849c2e9fe0b6f
hiramatch <hiramatch@gmail.com>
2019-05-02 18:23:22 +0900

    Error Styling

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -5,10 +5,15 @@
       placeholder="What's your email"
       v-model.trim="email"
       @blur="$v.email.$touch()"
+      :class="{ error: $v.email.$error }"
     />
     <div v-if="$v.email.$error">
-      <p class="errorMessage" v-if="!$v.email.email">Please enter a valid email address.</p>
-      <p class="errorMessage" v-if="!$v.email.required">Email is required.</p>
+      <p class="errorMessage" v-if="!$v.email.email">
+        Please enter a valid email address.
+      </p>
+      <p class="errorMessage" v-if="!$v.email.required">
+        Email is required.
+      </p>
     </div>
     <button type="submit">Submit</button>
   </div>
